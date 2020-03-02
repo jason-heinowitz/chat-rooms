c2d0f048f88c1ace5dc8554556063935ce5b360d
Jason Heinowitz <jasonheinowitz@yahoo.com>
2020-03-02 11:30:42 -0500

    Global messaging functional

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const moment = require('moment');
 
 const PORT = 25565;
 const users = {};
@@ -12,12 +13,26 @@ io.on('connection', (socket) => {
   console.log(`Connected [${user}][${socketID}]`);
   users[user] = { id: socketID, currentRoom: 'Global' };
   console.log(users);
-
   socket.join(users[user].currentRoom);
+
+  socket.on('message out', (msg) => {
+    console.log(`Message from ${user}: ${msg}`);
+    const messageObj = {
+      to: users[user].currentRoom,
+      msg: {
+        time: moment().format('MMMM Do YYYY, h:mm:ss a'), // March 2nd 2020, 8:51:52 am
+        user: users[user].id,
+        content: msg,
+      },
+    };
+    console.log(messageObj);
+    io.in(users[user].currentRoom).emit('message', messageObj);
+  });
 
   socket.on('disconnect', () => {
     console.log(`Disconnected ${user}`);
     delete users[user];
+    console.log(users);
   });
 });
 
